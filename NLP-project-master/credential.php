a5aa3ef39348af05fd3b8aa718ae4706d5bc3927
Shalaj17 <shalajkumaryadav@gmail.com>
2018-11-30 15:58:38 +0530

    First NLP-Project

---
+++ NLP-project-master/credential.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "shalaj";
+$password = "";
+$dname = "chemword";
+
+$connectq = new mysqli($servername, $username, $password, $dname);
+
+$connection = mysqli_connect($servername, $username, $password, $dname);
+
+
+if(mysqli_connect_errno()){
+    
+}
+
+ 
+
+?>
